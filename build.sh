e1100b8e00d1f50b76b25291eaf0ba1d6e70af2b
AndrewNeo <andrew@andrewneo.com>
2012-06-10 20:02:17 -0400

    Fixing modified path changes

--- build.sh
+++ build.sh
@@ -1,2 +1,3 @@
-coffee --join js/myconbook.mobile.js --compile cs/core.coffee cs/buildingmaps.coffee cs/coninfo.coffee cs/dealers.coffee cs/guide.coffee cs/guide-types.coffee cs/hotels.coffee cs/schedule.coffee
-~/node_modules/.bin/uglifyjs js/myconbook.mobile.js > js/myconbook.mobile.min.js
+
+coffee --join www/js/myconbook.mobile.js --compile cs/core.coffee cs/buildingmaps.coffee cs/coninfo.coffee cs/dealers.coffee cs/guide.coffee cs/guide-types.coffee cs/hotels.coffee cs/schedule.coffee
+uglifyjs www/js/myconbook.mobile.js > www/js/myconbook.mobile.min.js
